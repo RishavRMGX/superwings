01e3ea429dc488f7587e9e7d834c06935be8fdf1
RishavRMGX <rishav.mishra@rmgx.in>
2021-12-15 12:54:05 +0530

    Java Test Cases Added

--- JavaAssignment4/src/main/java/Main.java
+++ JavaAssignment4/src/main/java/Main.java
@@ -3,22 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        TaxiFare taxiFare = new TaxiFare();
         Scanner scan = new Scanner(System.in);
         System.out.printf("Please enter total distance traveled. (In Km only)");
         float distance = scan.nextFloat();
-        float total = getFareForDistance(distance);
+        float total = taxiFare.getFareForDistance(distance);
 
         if(total<0) System.out.printf("Invalid Distance");
         else System.out.printf("Total Fare for distance "+distance + "Km traveled : " + total + "Rs");
     }
 
-    private static Float getFareForDistance(float distance) {
-        int firstFiveKm = 5 * 20;
-        int tenKmAfterFirstFive = 10 * 18;
-        if(distance<0) return -1f;
-        if(distance <= 2) return 50.0f;
-        else if(distance <= 5) return distance * 20;
-        else if(distance <= 15) return  firstFiveKm + (distance-5) * 18;
-        else return firstFiveKm + tenKmAfterFirstFive + (distance - 15) * 15;
-    }
+
 }
--- JavaAssignment4/src/test/java/JavaTest.java
+++ JavaAssignment4/src/test/java/JavaTest.java
@@ -5,7 +5,47 @@ public class JavaTest {
 
     @Test
     void checkValidity(){
-        Assertions.assertEquals(1,1);
+        TaxiFare taxiFare = new TaxiFare();
 
+        //Inserting invalid negative distance
+        Assertions.assertTrue(taxiFare.getFareForDistance(-10)<0);
+    }
+
+    @Test
+    void checkForLessThenTwoKm(){
+        TaxiFare taxiFare = new TaxiFare();
+
+        //Inserting distance less then 2 km
+        Assertions.assertEquals(taxiFare.getFareForDistance(1.5f),50);
+    }
+
+    @Test
+    void checkForLessThenFiveKm(){
+        TaxiFare taxiFare = new TaxiFare();
+
+        //Inserting distance less than 5 km
+        float distance = 4.5f;
+        float result = distance*20;
+        Assertions.assertEquals(taxiFare.getFareForDistance(distance),result);
+    }
+
+    @Test
+    void checkForLessThenFifteenKm(){
+        TaxiFare taxiFare = new TaxiFare();
+
+        //Inserting distance less than 15 km and greater than 5
+        float distance = 14.5f;
+        float result = (5*20)+(distance-5)*18;
+        Assertions.assertEquals(taxiFare.getFareForDistance(distance),result);
+    }
+
+    @Test
+    void checkForGreaterThenFifteenKm(){
+        TaxiFare taxiFare = new TaxiFare();
+
+        //Inserting distance less than 15 km and greater than 5
+        float distance = 24.5f;
+        float result = (5*20)+10*18+(distance-15)*15;
+        Assertions.assertEquals(taxiFare.getFareForDistance(distance),result);
     }
 }
