e12056349c6b6580abbc9630829b4a1cd9281465
RishavRMGX <rishav.mishra@rmgx.in>
2021-12-22 12:05:04 +0530

    C# Assignment4 added

---
+++ C#Assignment4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter total distance traveled. (In Km only): ");
+
+TaxiFare taxiFare = new TaxiFare();
+String? input = Console.ReadLine();
+if(input==null ){
+    Console.WriteLine("Distance Not Found");
+}else {
+    double distance = Single.Parse(input);
+    double fare = taxiFare.getFare(distance);
+    if(fare<0) Console.WriteLine("Invalid Distance");
+    else Console.WriteLine("Total fare for distance traveled "+distance+" Km is "+fare+ "Rs");
+}
+
+
+
+
+
